9bc97cdc245693cda1c33dbbb61c259b3ada9c29
Marcelo Martins <marcelo.developer@outlook.com>
2018-11-03 18:54:21 -0300

    Arquivos da aula 31, seção 3

---
+++ ex.11/index.jsx
@@ -0,0 +1,8 @@
+//3.27 - Passando propriedades para Componentes filhos
+import React from 'react'
+import ReactDOM from 'react-dom'
+import ClassComponent from './classComponent'
+
+ReactDOM.render(
+    <ClassComponent value='Componente Classe'/>
+, document.getElementById('app'))
